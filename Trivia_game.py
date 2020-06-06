d8962e3b6a0a41b42027fe9830d9211af6eec080
Abhimanyu Mehta <66494125+Abhimayu-Mehta@users.noreply.github.com>
2020-06-06 13:15:54 +0530

    Create Trivia_game.py

---
+++ Trivia_game.py
@@ -0,0 +1,470 @@
+
+def gk_que_1():
+    print("QUE 1\n")
+    print("Capital of Australia\n")
+
+    a = input("enter the answer\n")
+
+    if a == "canberra":
+        print("well done")
+        gk_que_2()
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+
+
+def gk_que_2():
+
+    print("QUE 2\n")
+
+    print("World's biggest Island\n")
+
+    b = input("enter the answer\n")
+
+    if b == "greenland":
+        print("welldone")
+        gk_que_3()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+
+def gk_que_3():
+    print("QUE 3\n")
+
+    print("World's largest ocean\n")
+
+    c = input("enter the answer\n")
+
+    if c == "pacific":
+        print("welldone")
+        gk_que_4()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+def gk_que_4():
+    print("QUE 4\n")
+
+    print("What is the capital city of Spain?\n")
+
+    d = input("enter the answer\n")
+
+    if d == "madrid":
+        print("welldone")
+        gk_que_5()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+def gk_que_5():
+    print("QUE 5\n")
+
+    print("What is the diameter of Earth?\n")
+
+    e = input("enter the answer\n")
+
+    if e == "8000 miles":
+        print("welldone")
+        z = input("do you want to play again")
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+        if z == "yes":
+          print(gk_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+
+def sp_que_1():
+    print("QUE 1\n")
+    print("Which sport does Constantino Rocca play?\n")
+    f = input("enter the answer\n")
+    if f == "golf":
+        print("well done")
+        sp_que_2()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+
+def sp_que_2():
+    print("QUE 2\n")
+
+    print(
+        "In 2011, which country hosted a Formula 1 race for the first time?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "india":
+        print("well done")
+        sp_que_3()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+def sp_que_3():
+    print("QUE 3\n")
+
+    print("The Olympics are held every how many years?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "4 years":
+        print("well done")
+        sp_que_4()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sp_que_4():
+    print("QUE 4\n")
+
+    print("How many players are on a baseball team?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "9 players":
+        print("well done")
+        sp_que_5()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sp_que_5():
+    print("QUE 5\n")
+
+    print("What sport is dubbed the ‘king of sports’?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "soccer":
+        print("well done")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sp_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def gg_que_1():
+    print("QUE 1\n")
+
+    print("What island does the Statue of Liberty stand on?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "liberty island":
+        print("well done")
+        gg_que_2()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def gg_que_2():
+    print("QUE 2\n")
+
+    print("What nation produces two thirds of the world’s vanilla?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "madagascar":
+        print("well done")
+        gg_que_3()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def gg_que_3():
+    print("QUE 3\n")
+
+    print("What mountains on India's Northern border are nicknamed 'Snow Abode'?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "himalayas":
+        print("well done")
+        gg_que_4()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def gg_que_4():
+    print("QUE 4\n")
+
+    print("Which country has the most volcanoes?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "indonesia":
+        print("well done")
+        gg_que_5()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def gg_que_5():
+    print("QUE 5\n")
+
+    print("Portugal is bordered by what other country?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "spain":
+        print("well done")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(gg_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sc_que_1():
+    print("QUE 1\n")
+
+    print("What melted rock eventually becomes lava?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "magma":
+        print("well done")
+        sc_que_2()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sc_que_2():
+    print("QUE 2\n")
+
+    print("How many hearts does a octopus have?\n")
+
+    f = input("enter the answer\n")
+
+    if f == "3 hearts":
+        print("well done")
+        sc_que_3()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sc_que_3():
+    print("QUE 3\n")
+
+    print("which animal has the largest brain\n")
+
+    f = input("enter your answer\n")
+
+    if f == "sperm whale":
+        print("well done")
+        sc_que_4()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sc_que_4():
+    print("QUE 4\n")
+
+    print("What is the only bird that can fly backwards?\n")
+
+    f = input("enter your answer\n")
+
+    if f == "hummingbird":
+        print("well done")
+        sc_que_5()
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+def sc_que_5():
+    print("QUE 5\n")
+
+    print("What is the largest internal organ of the human body?\n")
+
+    f = input("Enter your answer\n")
+
+    if f == "liver":
+        print("well done")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+    else:
+        print("sorry")
+        z = input("do you want to play again")
+
+        if z == "yes":
+          print(sc_que_1())
+
+        else:
+          print("thankyou for playing")
+
+
+
+category = input(
+    "Which category of quetions you want to answer:\n1.general knowladge \n2.sports \n3.geography \n4.science\n")
+
+if category == "1":
+    print("General Knowladge questions coming")
+    gk_que_1()
+
+elif category == "2":
+    print("sports questions coming")
+    sp_que_1()
+
+elif category == "3":
+  print("Geography questions coming")
+  gg_que_1()
+
+elif category == "4":
+  print("Science questions coming")
+  sc_que_1()
